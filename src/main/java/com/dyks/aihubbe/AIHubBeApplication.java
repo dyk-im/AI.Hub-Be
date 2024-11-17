58082df5fd4da83b7df40607a8f06d73f315920a
Dongyun Kim <djskaldrjfj@naver.com>
2024-11-18 03:16:56 +0900

    first commit

---
+++ src/main/java/com/dyks/aihubbe/AIHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.dyks.aihubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIHubBeApplication.class, args);
+	}
+
+}
