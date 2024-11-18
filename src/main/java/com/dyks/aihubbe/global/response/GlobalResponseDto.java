897fefdcae7c4d1901481aa98c981ec4f48b7218
Dongyun Kim <djskaldrjfj@naver.com>
2024-11-18 20:45:52 +0900

    feat:globalexceptionhandler

--- src/main/java/com/dyks/aihubbe/global/response/GlobalResponseDto.java
+++ src/main/java/com/dyks/aihubbe/global/response/GlobalResponseDto.java
@@ -2,7 +2,11 @@ package com.dyks.aihubbe.global.response;
 
 import java.time.LocalDateTime;
 
+import com.dyks.aihubbe.global.error.ErrorCode;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -62,10 +66,26 @@ public class GlobalResponseDto<T> {
 	public static <T> GlobalResponseDto<T> fail(ErrorCode code, String message) {
 		return GlobalResponseDto.<T>builder()
 			.isSuccess(false)
+			.code(code.getStatus())
+			.timestamp(LocalDateTime.now())
+			.message(message)
+			.result(null)
+			.build();
+	}
+
+	public static <T> GlobalResponseDto<T> fail(int code, String message) {
+		return GlobalResponseDto.<T>builder()
+			.isSuccess(false)
 			.code(code)
 			.timestamp(LocalDateTime.now())
 			.message(message)
 			.result(null)
 			.build();
+	}
+
+	public String toJson() throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		return mapper.writeValueAsString(this);
 	}
 }
